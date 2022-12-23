6bf3dd5acfee4ddb63d53c023ef1c95f635b7ec2
ilsyaa <ilham.shindou@gmail.com>
2022-12-23 13:16:49 +0700

    fix claim license

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -19,36 +19,7 @@
                             <div class="badge rounded-pill badge-light-primary">6 New</div>
                         </div>
                     </li>
-                    <li class="scrollable-container media-list"><a class="d-flex" href="#">
-                            <div class="list-item d-flex align-items-start">
-                                <div class="me-1">
-                                    <div class="avatar"><img src="{{ asset('adminpanel') }}/images/portrait/small/avatar-s-15.jpg" alt="avatar" width="32" height="32"></div>
-                                </div>
-                                <div class="list-item-body flex-grow-1">
-                                    <p class="media-heading"><span class="fw-bolder">Congratulation Sam 🎉</span>winner!</p><small class="notification-text"> Won the monthly best seller badge.</small>
-                                </div>
-                            </div>
-                        </a><a class="d-flex" href="#">
-                            <div class="list-item d-flex align-items-start">
-                                <div class="me-1">
-                                    <div class="avatar"><img src="{{ asset('adminpanel') }}/images/portrait/small/avatar-s-3.jpg" alt="avatar" width="32" height="32"></div>
-                                </div>
-                                <div class="list-item-body flex-grow-1">
-                                    <p class="media-heading"><span class="fw-bolder">New message</span>&nbsp;received</p><small class="notification-text"> You have 10 unread messages</small>
-                                </div>
-                            </div>
-                        </a><a class="d-flex" href="#">
-                            <div class="list-item d-flex align-items-start">
-                                <div class="me-1">
-                                    <div class="avatar bg-light-danger">
-                                        <div class="avatar-content">MD</div>
-                                    </div>
-                                </div>
-                                <div class="list-item-body flex-grow-1">
-                                    <p class="media-heading"><span class="fw-bolder">Revised Order 👋</span>&nbsp;checkout</p><small class="notification-text"> MD Inc. order updated</small>
-                                </div>
-                            </div>
-                        </a>
+                    <li class="scrollable-container media-list">
                         <div class="list-item d-flex align-items-center">
                             <h6 class="fw-bolder me-auto mb-0">System Notifications</h6>
                             <div class="form-check form-check-primary form-switch">
